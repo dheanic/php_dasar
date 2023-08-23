7b135a7b13e4f9e4bc2659ba91d633e036f09c86
dheanic <dheanickyta71@gmail.com>
2023-08-23 13:55:58 +0700

    update

--- LKPD/lkpd4.php
+++ LKPD/lkpd4.php
@@ -1,23 +1,12 @@
 <?php
-if(isset($_POST['submit'])){
-    //pengisian input ke variable
-    $waktu = $_POST['times'];
-//decision
-if($waktu >= 3600){
-    // floor untuk membulatkan kebawah, veil membulatkan keatas, round membulatkan keatas dan kebawah
-    $jam = floor($waktu / 3600);
-    $waktu -= ($jam * 3600);
-    $hasil .= $jam . " jam ";
-    // operator += bersifat menambah, kalau hanya = bersifat menimpa
-}
-else if($waktu >= 60){
-    $menit = floor($waktu / 60);
-    $waktu -= ($menit * 60);
-    $hasil .= $menit . " menit ";
-}
-    $detik = $waktu;
-    $hasil .= $detik . " detik "; 
 
-echo $hasil;
-}
+  $td = 5440;
+
+  $jam = $td / 3600;
+  $menit = ($td % 3600) / 60; 
+  $detik = $td % 60;
+
+  echo "Jam: " .$jam;
+  echo "Menit: " .$menit; 
+  echo"Detik: " .$detik;
 ?>
